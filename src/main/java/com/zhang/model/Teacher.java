5fee6dc52e5b55629c2886d493ea331d7c955e1c
EricZhangGang <3278967865@qq.com>
2019-09-06 22:39:12 +0800

    teacher

--- src/main/java/com/zhang/model/Teacher.java
+++ src/main/java/com/zhang/model/Teacher.java
@@ -13,4 +13,60 @@ public class Teacher {
     private String position;//部门
     private String course;//科目
     private String phone;//联系方式
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
